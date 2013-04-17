db61796f311b19cffaa1b4a440003206f601d678
Dominic Hamon <dma+github@stripysock.com>
2013-04-17 13:01:54 -0700

    New dart:io interface requirements

--- dartrepl.dart
+++ dartrepl.dart
@@ -4,55 +4,57 @@ const String in_prompt = '>> ';
 const String out_prefix = '<< ';
 
 void main() {
-  final StringInputStream stream = new StringInputStream(stdin);
   final List<String> lines = new List<String>();
   final File tmpfile = new File('.dartrepl');
   final String dartvm = new Options().executable;
 
-  stdout.writeString(in_prompt);
-  stdout.flush();
-  stream.onLine = () {
-    var line = stream.readLine();
-    if (line != null) {
-      // TODO: Special commands for listing current program, editing lines, etc.
+  stdout.write(in_prompt);
+  //stdout.flush();
+  stdin
+    .transform(new StringDecoder())
+    .transform(new LineTransformer())
+    .listen((String line) {
+        // TODO: Special commands for listing current program, editing lines, etc.
 
-      // add line to list
-      lines.add(line);
+        // add line to list
+        lines.add(line);
 
-      // write lines to file
-      var tmpstream = tmpfile.openOutputStream(FileMode.WRITE);
-      lines.forEach((l) { tmpstream.writeString("$l\n"); });
-      tmpstream.flush();
+        // write lines to file
+        var tmpstream = tmpfile.openWrite();
+        lines.forEach((l) { tmpstream.writeln("$l"); });
+        //tmpstream.flush();
 
-      // Spawn DART VM process with file as arg
-      Process.start(dartvm, [tmpfile.fullPathSync()])
-        ..then((p) => vm_running(p, lines))
-        ..catchError((e) => vm_error(e));
-    }
-  };
+        // Spawn DART VM process with file as arg
+        Process.start(dartvm, [tmpfile.fullPathSync()])
+          ..then((p) => vm_running(p, lines))
+          ..catchError((e) => vm_error(e));
+    });
 }
 
 void vm_running(Process p, List<String> lines) {
-  var stdoutStream = new StringInputStream(p.stdout);
-  stdoutStream.onLine =
-      () => stdout.writeString("  $out_prefix ${stdoutStream.readLine()}\n");
-  p.onExit = (exitCode) {
-    if (exitCode != 0) {
-      stderr.writeString('[error] ');
-      stderr.write(p.stderr.read());
-    } else {
-      stdout.writeString('[success]\n');
-      lines.clear();
-    }
-    int i = 1;
-    lines.forEach((l) { stdout.writeString(" $i. $l\n"); ++i; });
-    p.kill(ProcessSignal.SIGQUIT);
-    stdout.writeString(in_prompt);
-    stdout.flush();
-  };
+  p.stdout
+    .transform(new StringDecoder())
+    .transform(new LineTransformer())
+    .listen((String line) {
+        stdout.writeln("  $out_prefix $line");
+    });
+  p.exitCode.then((exitCode) {
+      if (exitCode != 0) {
+        stderr.write('[error] ');
+        stderr.addStream(p.stderr);
+      } else {
+        stdout.writeln('[success]');
+        lines.clear();
+      }
+      int i = 1;
+      lines.forEach((l) { stdout.writeln(" $i. $l"); ++i; });
+      p.kill(ProcessSignal.SIGQUIT);
+      stdout.write(in_prompt);
+      //stdout.flush();
+  });
 }
 
 bool vm_error(e) {
-  stderr.writeString('Failed to start VM: ${e.message}\n');
+  stderr.writeln('Failed to start VM: ${e.message}');
   return true;
 }
